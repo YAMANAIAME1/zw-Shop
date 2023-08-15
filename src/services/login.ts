22fea75c9c034a3660056d64a1141e6acc16b52a
xswle <361039005@qq.com>
2023-08-15 23:45:27 +0800

    feat:完成登录模块

---
+++ src/services/login.ts
@@ -0,0 +1,29 @@
+import type { LoginResult } from '@/types/member'
+import { http } from '@/utils/http'
+
+type LoginParams = {
+  code: string
+  encryptedData: string
+  iv: string
+}
+export const postLoginWxMin = (data: LoginParams) => {
+  return http<LoginResult>({
+    url: '/login/wxMin',
+    method: 'POST',
+    data,
+  })
+}
+
+/**
+ * 小程序登录_内测版
+ * @param phoneNumber 模拟手机号码
+ */
+export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
+  return http<LoginResult>({
+    method: 'POST',
+    url: '/login/wxMin/simple',
+    data: {
+      phoneNumber,
+    },
+  })
+}
