9f33572c6f0666084ee97685d2a98eaadf27bc71
xswle <361039005@qq.com>
2023-08-15 23:17:45 +0800

    feat:完成商品详情功能

---
+++ src/pages/goods/components/PageSkeleton.vue
@@ -0,0 +1,221 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <scroll-view :scroll-y="true" class="viewport" :enable-back-to-top="true">
+      <view class="goods">
+        <view class="preview">
+          <swiper :circular="true" :current="0" :autoplay="false">
+            <swiper-item
+              style="
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                transform: translate(0%, 0px) translateZ(0px);
+              "
+            >
+              <image mode="aspectFill" class="sk-image"></image>
+            </swiper-item>
+          </swiper>
+          <view class="indicator">
+            <text class="current sk-transparent sk-opacity">1</text>
+            <text class="split sk-transparent sk-opacity">/</text>
+            <text class="total sk-transparent sk-opacity">5</text>
+          </view>
+        </view>
+        <view class="meta">
+          <view class="price">
+            <text class="symbol sk-transparent sk-opacity">¥</text>
+            <text class="number sk-transparent sk-text-14-2857-626 sk-text">1499.00</text>
+          </view>
+          <view class="name ellipsis sk-transparent sk-text-14-2857-962 sk-text"
+            >飞天53%vol500ml贵州茅台酒（带杯）</view
+          >
+          <view class="desc sk-transparent sk-text-0-0000-336 sk-text">茅台飘香，正品溯源app</view>
+        </view>
+        <view class="action">
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-168 sk-text">选择</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-783 sk-text">
+              请选择商品规格
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle">
+            <text class="label sk-transparent sk-text-14-2857-894 sk-text">送至</text>
+            <text class="text ellipsis sk-transparent sk-text-14-2857-262 sk-text">
+              请选择收获地址
+            </text>
+          </view>
+          <view class="item arrow sk-pseudo sk-pseudo-circle"></view>
+        </view>
+      </view>
+    </scroll-view>
+    <view class="toolbar" style="padding-bottom: 0px">
+      <view class="icons">
+        <button class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle">
+          <text class="icon-heart sk-pseudo sk-pseudo-circle"></text>收藏
+        </button>
+        <button
+          class="icons-button sk-transparent sk-button sk-pseudo sk-pseudo-circle"
+          open-type="contact"
+        >
+          <text class="icon-handset sk-pseudo sk-pseudo-circle"></text>客服
+        </button>
+        <navigator class="icons-button sk-transparent" open-type="switchTab">
+          <text class="icon-cart sk-pseudo sk-pseudo-circle"></text>购物车
+        </navigator>
+      </view>
+      <view class="buttons">
+        <view
+          class="addcart sk-transparent sk-text-31-9444-305 sk-text"
+          style="background-position-x: 50%"
+        >
+          加入购物车
+        </view>
+        <view
+          class="buynow sk-transparent sk-text-31-9444-358 sk-text"
+          style="background-position-x: 50%"
+        >
+          立即购买
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style>
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-626 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 78.4rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-14-2857-962 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-336 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 24rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-168 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-783 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-894 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-262 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-31-9444-305 {
+  background-image: linear-gradient(
+    transparent 31.9444%,
+    #eeeeee 0%,
+    #eeeeee 68.0556%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 72rpx;
+  position: relative !important;
+}
+.sk-text-31-9444-358 {
+  background-image: linear-gradient(
+    transparent 31.9444%,
+    #eeeeee 0%,
+    #eeeeee 68.0556%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 72rpx;
+  position: relative !important;
+}
+.sk-button {
+  color: #efefef !important;
+  background: #efefef !important;
+  border: none !important;
+  box-shadow: none !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
